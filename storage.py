13440bba64f598d57b92f02ba156380e5c8895e1
Евгений Кравцов <evgenijkravcov@Evgenijs-MacBook-Pro.local>
2022-11-25 18:22:48 +0800

    Heroku Postgres initial integration

--- storage.py
+++ storage.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import os
 import psycopg2
 
 
@@ -62,5 +63,46 @@ class LocalPostgresDatabase(Storage):
 
 class HerokuPostgresDatabase(Storage):
 
-    def get_player_rating(player_name):
-        pass
+    def get_player_rating(self, player_name):
+        connection = self.__connect()
+
+        try:
+            cursor = connection.cursor()
+            cursor.execute(
+                "SELECT rating FROM players WHERE name = %s", (player_name, ))
+
+            rating = cursor.fetchall()
+
+            if not rating:
+                cursor.execute(
+                    "INSERT INTO players (name, rating) VALUES (%s, %s)", (player_name, 1500))
+                connection.commit()
+                cursor.close()
+                return 1500
+            else:
+                cursor.close()
+                return rating[0][0]
+        finally:
+            if connection is not None:
+                connection.close()
+
+    def update_player_rating(self, player_name, rating):
+        connection = self.__connect()
+
+        try:
+            cursor = connection.cursor()
+            cursor.execute(
+                "UPDATE players SET rating = %s WHERE name = %s", (rating, player_name))
+            connection.commit()
+            cursor.close()
+        finally:
+            if connection is not None:
+                connection.close()
+
+    def __connect(self):
+        DATABASE_URL = os.environ["DATABASE_URL"]
+        connection = None
+        try:
+            return psycopg2.connect(DATABASE_URL, sslmode='require')
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
