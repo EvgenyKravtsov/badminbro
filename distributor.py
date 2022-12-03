007c7aa2fc6b6c3f644e48ec49f3e2976b408826
Denis Novikov <denis.novikov@picsart.com>
2022-12-03 08:03:43 +0800

    sorting algorithm v1.2 minor fix

--- distributor.py
+++ distributor.py
@@ -2,12 +2,12 @@ import itertools
 
 
 def distribute_players_for_match(players):
-
-    fresh_players_number = [i for i in range(len(players)) if players[i].participation_in_the_last_game == False]
-    played_players_number = [i for i in range(len(players)) if players[i].participation_in_the_last_game == True]
-
+    
     players_in_game = []  # list of players in current game
     players_number_in_game = []  # list for creating permutation set
+
+    # first choose two played players
+    played_players_number = [i for i in range(len(players)) if players[i].participation_in_the_last_game == True]
 
     # find players with min games played in a row
     min_games_count = []
@@ -22,15 +22,21 @@ def distribute_players_for_match(players):
 
     while len(min_games_count) > 0:
         for i in range(len(played_players_number)):
-            if players[played_players_number[i]].games_played_in_a_row == min_games_count[0]:
-                players_number_in_game.append(played_players_number[i])
-                players_in_game.append(players[played_players_number[i]])
+            j = len(played_players_number) - i - 1
+            if players[played_players_number[j]].games_played_in_a_row == min_games_count[0]:
+                players_in_game.append(players[played_players_number[j]])
+                players_number_in_game.append(played_players_number.pop(j)) 
                 del min_games_count[0]
                 if len(min_games_count) == 0:
                     break
 
-    print('pig_1:', players_number_in_game)
+    for i in played_players_number:
+        players[i].participation_in_the_last_game = False
 
+    # SECOND choose fresh players
+    fresh_players_number = [i for i in range(len(players)) if players[i].participation_in_the_last_game == False]
+
+    # find players with min games played in a row
     all_games_count.clear()
     all_games_count = [players[i].games_played_in_a_row for i in fresh_players_number]
 
@@ -47,7 +53,6 @@ def distribute_players_for_match(players):
                 if len(min_games_count) == 0:
                     break
 
-    print('pig_2:', players_number_in_game)
 
     # find rank balance between teams
     team_rating_diff = abs((players_in_game[0].rating + players_in_game[1].rating) - (
@@ -65,46 +70,10 @@ def distribute_players_for_match(players):
     for player in players:
         player.participation_in_the_last_game = False
 
-
-    print(players_number_in_game)
-
     for number in players_number_in_game:
         players[number].games_played_in_a_row += 1
         players[number].participation_in_the_last_game = True
 
 
-    # # update players in rest list
-    # max_games = -1
-    # go_rest_players_numbers_list = []
-    # for _ in range(2):
-    #     for number in players_number_in_game:
-    #         if players[number].games_played_in_a_row > max_games:
-    #             max_games = players[number].games_played_in_a_row
-    #     for number in players_number_in_game:
-    #         if players[number].games_played_in_a_row == max_games:
-    #             go_rest_player = players_number_in_game.pop(
-    #                 players_number_in_game.index(number))
-    #             go_rest_players_numbers_list.append(go_rest_player)
-    #             break
-
-    # # find players with min games played in a row
-    # two_min_games_count = []
-    # all_games_count = [player.games_played_in_a_row for player in players]
-    # for _ in range(2):
-    #     min_count = min(all_games_count)
-    #     two_min_games_count.append(all_games_count.pop(
-    #         all_games_count.index(min_count)))
-
-    # players_number_in_rest = [i for i in range(len(
-    #     players)) if i != players_number_in_game[0] and i != players_number_in_game[1]]
-
-    # # update players in game list
-    # while len(two_min_games_count) > 0:
-    #     for i in range(len(players_number_in_rest)):
-    #         if players[players_number_in_rest[i]].games_played_in_a_row == two_min_games_count[0]:
-    #             players_number_in_game.append(players_number_in_rest[i])
-    #             del two_min_games_count[0]
-    #             if len(two_min_games_count) == 0:
-    #                 break
 
     return [players[players_number_in_game[i]] for i in range(4)]
